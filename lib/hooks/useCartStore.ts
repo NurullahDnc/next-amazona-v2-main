40cf567a459d2f168f6d208f283dbf7bf3bdec3c
NurullahDnc <150585098+NurullahDnc@users.noreply.github.com>
2024-07-14 00:25:54 +0300

    first commit

---
+++ lib/hooks/useCartStore.ts
@@ -0,0 +1,131 @@
+import { create } from "zustand";
+import { round2 } from "../utils";
+import { OrderItem, ShippingAddress } from "../models/OrderModel";
+import { persist } from "zustand/middleware";
+
+type Cart = {
+  items: OrderItem[];
+  itemsPrice: number;
+  taxPrice: number;
+  shippingPrice: number;
+  totalPrice: number;
+
+  paymentMethod: string;
+  shippingAddress: ShippingAddress;
+};
+
+// Sepet için başlangıç durumu
+const initialState: Cart = {
+  items: [],
+  itemsPrice: 0,
+  taxPrice: 0,
+  shippingPrice: 0,
+  totalPrice: 0,
+  paymentMethod: "PayPal",
+  shippingAddress: {
+    fullName: "",
+    address: "",
+    city: "",
+    postalCode: "",
+    country: "",
+  },
+};
+
+// Zustand kullanarak sepet durumunu oluşturma ve durumu kalıcı hale getirme
+export const cartStore = create<Cart>()(
+  persist(() => initialState, {
+    name: "cartStore",
+  })
+);
+
+export default function useCartService() {
+  const {
+    items,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    paymentMethod,
+    shippingAddress,
+  } = cartStore();
+  return {
+    items,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    paymentMethod,
+    shippingAddress,
+
+    // Sepete ürün ekleme - arttırma fonksiyonu
+    increase: (item: OrderItem) => {
+      const exist = items.find((x) => x.slug === item.slug);
+      const updatedCartItems = exist
+        ? items.map((x) =>
+            x.slug === item.slug ? { ...exist, qty: exist.qty + 1 } : x
+          )
+        : [...items, { ...item, qty: 1 }];
+      const { itemsPrice, shippingPrice, taxPrice, totalPrice } =
+        calcPrice(updatedCartItems);
+      cartStore.setState({
+        items: updatedCartItems,
+        itemsPrice,
+        shippingPrice,
+        taxPrice,
+        totalPrice,
+      });
+    },
+
+    // Sepetten ürün çıkarma-azaltma fonksiyonu
+    decrease: (item: OrderItem) => {
+      const exist = items.find((x) => x.slug === item.slug);
+      if (!exist) return;
+      const updatedCartItems =
+        exist.qty === 1
+          ? items.filter((x: OrderItem) => x.slug !== item.slug)
+          : items.map((x) =>
+              x.slug === item.slug ? { ...exist, qty: exist.qty - 1 } : x
+            );
+      const { itemsPrice, shippingPrice, taxPrice, totalPrice } =
+        calcPrice(updatedCartItems);
+      cartStore.setState({
+        items: updatedCartItems,
+        itemsPrice,
+        shippingPrice,
+        taxPrice,
+        totalPrice,
+      });
+    },
+
+    // Teslimat adresini kaydetme fonksiyonu
+    saveShippingAddrress: (shippingAddress: ShippingAddress) => {
+      cartStore.setState({
+        shippingAddress,
+      });
+    },
+
+    // Ödeme yöntemini kaydetme fonksiyonu
+    savePaymentMethod: (paymentMethod: string) => {
+      cartStore.setState({
+        paymentMethod,
+      });
+    },
+    clear: () => {
+      cartStore.setState({
+        items: [],
+      });
+    },
+    init: () => cartStore.setState(initialState),
+  };
+}
+
+// Fiyat hesaplama fonksiyonu, sepet öğelerinin toplam fiyatlarını hesaplar
+const calcPrice = (items: OrderItem[]) => {
+  const itemsPrice = round2(
+      items.reduce((acc, item) => acc + item.price * item.qty, 0)
+    ),
+    shippingPrice = round2(itemsPrice > 100 ? 0 : 100),
+    taxPrice = round2(Number(0.15 * itemsPrice)),
+    totalPrice = round2(itemsPrice + shippingPrice + taxPrice);
+  return { itemsPrice, shippingPrice, taxPrice, totalPrice };
+};
